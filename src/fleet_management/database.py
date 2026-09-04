2bfcbd3f2733518edc98ccc64dac546a5d90fdbb
Danylo Yakymiv <159173378+YakymivDanylo@users.noreply.github.com>
2026-09-04 16:06:51 +0300

    chore(core): integrate ruff for linting and formatting

--- src/fleet_management/database.py
+++ src/fleet_management/database.py
@@ -1,6 +1,7 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from .config import settings
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
+
+from .config import settings
 
 engine = create_async_engine(settings.database_url)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
